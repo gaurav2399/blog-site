c9e5230095432ba20685c19880ffc697b4fadf0c
Gaurav <gs926189@gmail.com>
2021-11-24 12:30:39 +0530

    add gitignore file

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 const _ = require('lodash');
-const secrets = require('./env/secrets');
 
 const homeStartingContent = "This is a blog webiste. Here you can create post by clicking on 'create post' button.";
 const aboutContent = "This website is design and develope by Gaurav Sharma.";
@@ -16,8 +15,7 @@ const app = express();
 
 app.use(express.static("public"));
 
-
-const mongo_url_cloud = "mongodb+srv://" + secrets.username + ":" + secrets.password + "@cluster0.udwj8.mongodb.net/blogDB?retryWrites=true&w=majority";
+const mongo_url_cloud = "mongodb+srv://admin-gaurav:gaurav%40sharma@cluster0.udwj8.mongodb.net/blogDB?retryWrites=true&w=majority";
 const mongo_url_local = "mongodb://localhost:27017/blogDB";
 
 mongoose.connect(mongo_url_cloud);
@@ -38,20 +36,25 @@ const Post = mongoose.model('Post',postSchema);
 app.use('/favicon.ico', express.static('public/blog-icon.svg'));
 
 app.get('/', function(req, res) {
-  Post.find({}, function(err, posts){
-    if(!err){
-      res.render('home', {
-        homePageContent: homeStartingContent,
-        postsData: posts
-      });
-    }else{
-      console.log('error while getting posts ' + err);
-      res.render('home', {
-        homePageContent: homeStartingContent,
-        postsData: []
-      });
-    }
-  });
+  try{
+    Post.find({}, function(err, posts){
+      if(!err){
+        res.render('home', {
+          homePageContent: homeStartingContent,
+          postsData: posts
+        });
+      }else{
+        console.log('error while getting posts ' + err);
+        res.render('home', {
+          homePageContent: homeStartingContent,
+          postsData: []
+        });
+      }
+    });
+  }catch(e){
+    console.log('error loading page ' + e);
+    res.send('503...Server side error...Please conatct to owner');
+  }
 });
 
 app.get('/about', function(req, res) {
